68dec4f35343351aab66912008d1475e191a4461
HazelPrince3 <jessicajjohnson37@gmail.com>
2025-05-08 17:31:33 +0100

    Create users table and insert data into the seed function

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,8 @@
 const db = require("./connection");
 const format = require("pg-format")
-const formatPropertyTypes = require("./utils/utils")
+const {formatPropertyTypes, formatUsers} = require("./utils/utils")
 
-async function seed(propertyTypesData, users) {
+async function seed(propertyTypesData, usersData) {
 
     await db.query(`DROP TABLE IF EXISTS users;`)
     await db.query(`DROP TABLE IF EXISTS property_types;`)
@@ -18,8 +18,21 @@ async function seed(propertyTypesData, users) {
             `INSERT INTO property_types (property_type, description) VALUES %L`, 
             formatPropertyTypes(propertyTypesData))
         );
-    
-   
+
+    await db.query(`CREATE TABLE users (
+        user_id SERIAL PRIMARY KEY,
+        first_name VARCHAR NOT NULL,
+        surname VARCHAR NOT NULL,
+        email VARCHAR NOT NULL,
+        phone_number VARCHAR,
+        is_host BOOLEAN NOT NULL, 
+        avatar VARCHAR,
+        create_at TIMESTAMP DEFAULT NOW());`);
+
+    await db.query(
+        format(
+             `INSERT INTO users (first_name, surname, email, phone_number, is_host, avatar) VALUES %L`, formatUsers(usersData))
+         );
 }
 
 module.exports = seed;
