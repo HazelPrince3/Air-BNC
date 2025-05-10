61934d191df80a9c6a4de78d91966004e659a258
HazelPrince3 <jessicajjohnson37@gmail.com>
2025-05-10 15:14:02 +0100

    Update the run seed file

--- db/seed-run.js
+++ db/seed-run.js
@@ -1,7 +1,7 @@
 const seed = require("./seed");
-const {propertyTypesData, usersData, propertiesData} = require("./data/test/");
+const {propertyTypesData, usersData, propertiesData, reviewsData} = require("./data/test/");
 const db = require("./connection")
 
-seed(propertyTypesData, usersData, propertiesData).then(() =>{
+seed(propertyTypesData, usersData, propertiesData, reviewsData).then(() =>{
     db.end()
 });
