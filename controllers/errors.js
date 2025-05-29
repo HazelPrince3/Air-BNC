c47e93b08ddf0364812e5b70d605be161ecb29d9
HazelPrince3 <jessicajjohnson37@gmail.com>
2025-05-29 15:01:29 +0100

    Create handleBadRequest and handleServerErrors

--- controllers/errors.js
+++ controllers/errors.js
@@ -2,5 +2,16 @@ exports.handlePathNotFound = (req, res, next) =>{
     res.status(404).send({msg: "Path not found."})}
 
 exports.handleCustomError = (err, req, res, next) => {
-    res.status(err.status).send({msg: err.msg})
+    if(err.status && err.msg){
+        res.status(err.status).send({msg: err.msg})
+    } else {next(err)}
+   
+}
+exports.handleBadRequest = (err, req, res, next) => {
+    res.status(400).send({msg: "Bad request."})
+    next(err)
+}
+exports.handleServerError = (err, req, res, next) => {
+    
+    res.status(500).send({msg: "Internal Server Error."})
 }
