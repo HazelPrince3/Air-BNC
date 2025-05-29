db5e124e94af9547417b80226c6888576abcde37
HazelPrince3 <jessicajjohnson37@gmail.com>
2025-05-29 14:59:50 +0100

    Update app file

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express")
-const {getProperties} = require("./controllers/properties.controllers")
+const {getProperties, getSingleProperty} = require("./controllers/properties.controllers")
 const {getPropertyReviews} = require("./controllers/reviews.controllers")
-const {handlePathNotFound, handleCustomError} = require("./controllers/errors")
+const {handlePathNotFound, handleCustomError, handleBadRequest, handleServerError} = require("./controllers/errors")
 const app = express()
 
 app.get("/api/properties", getProperties)
 
 app.get("/api/properties/:id/reviews", getPropertyReviews)
 
+app.get("/api/properties/:id", getSingleProperty)
+
 app.all("*invalid-path", handlePathNotFound)
 
 app.use(handleCustomError)
-
+app.use(handleBadRequest)
+app.use(handleServerError)
 module.exports = app
